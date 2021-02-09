4117672a738c86c47ac370b83ef946a888f616f7
morphwear <77355796+morphwear@users.noreply.github.com>
2021-02-09 13:45:28 -0800

    Create saveToFirebase.js

---
+++ assets/js/saveToFirebase.js
@@ -0,0 +1,15 @@
+function saveToFirebase(email) {
+  var emailObject = {
+    email: email
+  };
+  
+  firebase.database().ref('email-signup-entries').push().set(emailObject)
+    .then(function(snapshot)) {
+      success(); // some success method
+    }, function(error) {
+      console.log('error' + error);
+      error(); //some error method
+    }
+}
+
+saveToFirebase(email);
