6460285682c1cb5db1619716012cad47becebc6b
morphwear <77355796+morphwear@users.noreply.github.com>
2021-02-10 13:00:20 -0800

    Update saveToFirebase.js

--- assets/js/saveToFirebase.js
+++ assets/js/saveToFirebase.js
@@ -1,3 +1,23 @@
+// Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+  apiKey: "AIzaSyCAzQ8EnCrnBWb6mvxCnC49F1NVdnyXrhE",
+  authDomain: "mw-landing-site.firebaseapp.com",
+  projectId: "mw-landing-site",
+  storageBucket: "mw-landing-site.appspot.com",
+  messagingSenderId: "143218941547",
+  appId: "1:143218941547:web:3dc9366a2b7f63ffd0db2e",
+  measurementId: "G-LFB9KJEXNV"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+// Listen for form submit
+document.getElementById('footer-email-form').addEventListener('submit', submitForm);
+
+
 function saveToFirebase(email) {
   var emailObject = {
     email: email
@@ -5,11 +25,30 @@ function saveToFirebase(email) {
   
   firebase.database().ref('email-signup-entries').push().set(emailObject)
     .then(function(snapshot) {
-      success(); // some success method
+      success();
     }, function(error) {
       console.log('error' + error);
-      error(); //some error method
+      //error(); //some error method
     });
 }
 
-saveToFirebase(email);
+function success() {
+  // Show alert
+  document.querySelector('.alert').style.dsiplay = "block";
+};
+
+// Function to get form values
+function getInputVal(id) {
+  return document.getElementById(id).value;
+};
+
+
+function submitForm(e) {
+  e.preventDefault();
+  
+  // Get values
+  var email = getInputVal('email);
+  
+  // Save values                        
+  saveToFirebase(email);
+};
